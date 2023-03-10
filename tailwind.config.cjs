eea2dbf748a29fc6492f6e901f43840b3c1d7fe4
SejiMe <atienza.seji@gmail.com>
2023-03-10 21:13:26 +0800

    add font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 const config = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                // caveat: ["Caveat", fontFamily.sans],
+            },
+        },
     },
     plugins: [],
 };
